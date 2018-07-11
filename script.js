6a35f08efd9e0bf88cdcbb0e2f9bb6b001115ee0
Anna Jankowicz <reien.san@gmail.com>
2018-07-11 21:11:02 +0200

    Connected contact form with Google Forms, added Facebook link

--- script.js
+++ script.js
@@ -86,3 +86,17 @@ window.setInterval(()=>{
 		slider(galleries[i], 1);
 	}
 }, 5000);
+
+//Message after submitting form
+document.getElementById('gform').onsubmit = () => {
+	document.getElementById('msg').innerHTML = 'Thank you for your message';
+	document.getElementById('gform').style.display = 'none';
+	setTimeout(() => {
+		document.getElementById('msg').innerHTML = '';
+		[].forEach.call(document.getElementsByClassName('text-input'), input => {
+			input.value = '';
+		});
+		document.getElementById('gform').style.display = 'block';
+	}, 5000);
+
+};
