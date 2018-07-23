15b3178923829d657ecb5a8f74b53700bf5a3fb4
Anna Jankowicz <reien.san@gmail.com>
2018-07-23 20:50:28 +0200

    Fixed on-scroll animation timeout 2

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -4,15 +4,13 @@
 let preloader = document.getElementById('preloader');
 window.addEventListener('load', () => {
     setTimeout(() => {
-        preloader.classList.add('hidden');
-    }, 500);
+        preloader.classList.add('hidden');     
+    }, 100);
     setTimeout(() => {
         preloader.classList.add('display-none');
         document.getElementById('loading-icon').classList.add('paused');
-    }, 520);
-     //Activate header's elements' animations
-    setTimeout(() => {                
+        //Activate header's elements' animations
         document.getElementById('header').childNodes[1].classList.add('onscroll-animation');
         document.getElementById('header').childNodes[3].classList.add('onscroll-animation');
-    }, 570);
+    }, 120);
 });
