36dd07aaab9ff0e400101cc15b2a44e45f334975
Anna <reien.san@gmail.com>
2022-10-11 12:31:16 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,37 @@
-const gulp = require('gulp');
-const imageMin = require('gulp-imagemin');
-const uglify = require('gulp-uglify-es').default;
-const concat = require('gulp-concat');
-const sass = require('gulp-sass');
+import gulp from 'gulp';
+import imageMin from 'gulp-imagemin';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
+import gulpSass from 'gulp-sass';
+import dartSass from 'sass';
 
-gulp.task('copyHtml', function(){
+const sass = gulpSass(dartSass)
+
+gulp.task('copyHtml', async function(){
     gulp.src('src/*.html')
-        .pipe(gulp.dest(''));
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('imageMin', function(){
+gulp.task('imageMin', async function(){
     gulp.src('src/assets/*')
         .pipe(imageMin())
         .pipe(gulp.dest('assets'));
 });
 
-gulp.task('minify', function(){
+gulp.task('minify', async function(){
     gulp.src('src/js/*.js')
         .pipe(concat('script.js'))
         .pipe(uglify())
         .pipe(gulp.dest('js'));
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', async function(){
     gulp.src('src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['copyHtml', 'imageMin', 'minify', 'sass']);
+gulp.task('default', gulp.series('copyHtml', 'imageMin', 'minify', 'sass'));
 
 gulp.task('watch', function(){
     gulp.watch('src/*.html', ['copyHtml']);
